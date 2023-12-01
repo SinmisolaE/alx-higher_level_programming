7b0772e5d8211fa9cff2a67a49daf7272a138e4b
SinmisolaE <sinmisolaajao@gmail.com>
2023-12-01 13:11:40 -0800

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2
+def new_in_list(my_list, idx, element):
+    list = my_list
+    if idx < 0:
+        return list
+    elif idx >= len(list):
+        return list
+    else:
+        list[idx] = element
+        return list
