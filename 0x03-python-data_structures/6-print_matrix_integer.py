7e31e0ecdc73ea6544d69fa9e01f1167c75f54af
SinmisolaE <sinmisolaajao@gmail.com>
2023-12-02 13:20:40 -0800

    1-element_at.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix and matrix[] is None:
+    if matrix[] is None:
         return
     for i in range(len(matrix)):
         for x in range(len(matrix[i])):
