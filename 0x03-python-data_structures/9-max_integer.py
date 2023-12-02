c957844e03371a1b7bd574b5df9a1ebd7e6dafae
SinmisolaE <sinmisolaajao@gmail.com>
2023-12-02 14:03:33 -0800

    1-element_at.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     x = 0
     if my_list is None:
-        return
+        return None
     for i in my_list:
         if i > x:
-            x = i
+            x = 
     return x
