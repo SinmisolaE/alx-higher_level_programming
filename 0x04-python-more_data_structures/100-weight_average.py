de14da18ab089501fa3d8f0010ee69d64a730bb0
SinmisolaE <sinmisolaajao@gmail.com>
2023-12-08 13:39:13 -0800

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is None:
+        return 0
+    sum = 0
+    div = 0
+    for i in my_list:
+        mul = i[0] * i[1]
+        sum += mul
+        div += i[1]
+    result = sum / div
+    return result
