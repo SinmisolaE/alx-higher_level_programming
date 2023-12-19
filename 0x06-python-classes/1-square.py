6e98e63919936de3b80b131649a8e99e57c2fc37
SinmisolaE <sinmisolaajao@gmail.com>
2023-12-19 14:56:13 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/phyton3
+
+"""Define a class Square"""
+
+
+class Square:
+    """Represents a Square"""
+
+    def __init__(self, size=0):
+        """Initializes the size of square
+        type = int
+        """
+        self.__size = size
