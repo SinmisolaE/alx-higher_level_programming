41b83fc54689bb2d4a51b7cbf963cf731ba36e8d
SinmisolaE <sinmisolaajao@gmail.com>
2023-12-19 18:55:46 -0800

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,7 +50,7 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        [print("") for i in range(self.__position[1])]
+        [print("") for i in range(0, self.__position[1])]
 
         for c in range(self.__size):
             [print(" ", end="") for b in range(self.__position[0])]
