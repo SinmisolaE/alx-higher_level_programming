d668904f0753cad2515013273041e2a2ef042497
SinmisolaE <sinmisolaajao@gmail.com>
2024-01-06 11:43:28 -0800

    101-nqueens.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """ Initializing attributes """
         type(self).number_of_instances += 1
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def height(self):
