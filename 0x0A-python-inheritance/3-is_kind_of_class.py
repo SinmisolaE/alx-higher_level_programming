1ec8999409ba9ab66bef29e9e22e99b07b9ba2e3
SinmisolaE <sinmisolaajao@gmail.com>
2024-01-08 07:58:01 -0800

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defines a method is_kind_of_class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ returns True if the object is an instance of
+    or if the object is an instance of a class that inherited from """
+    if isinstance(obj, a_class):
+        return True
+    return False
