b93fbe463b5d4086009c55feec353202138b7168
SinmisolaE <sinmisolaajao@gmail.com>
2024-01-08 14:21:01 -0800

    8-rectangle.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Defines class BaseGeometry """
+
+
+class BaseGeometry:
+    """ The Base class """
+    def area(self):
+        """ Area method that raises error if no message passes into self """
+        if not vars(self):
+            raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates value """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
