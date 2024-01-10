2941071f024167ffb59f2d2b92619f3ea1782126
SinmisolaE <sinmisolaajao@gmail.com>
2024-01-10 12:52:29 -0800

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Define a class Student """
+
+
+class Student:
+    """ Defines a student """
+    def __init__(self, first_name, last_name, age):
+        """ Initializes attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Returns dictionary representation
+        of Student instance"""
+        if (type(attrs) == list and
+           all(type(x) == str for x in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ that replaces all attributes of the Student instance"""
+        for x, i in json.items():
+            setattr(self, x, i)
