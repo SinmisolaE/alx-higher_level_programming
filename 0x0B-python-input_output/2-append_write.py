d0ccd1dc5ac9bd7c9f5e3fa28bdd603d38e9b485
SinmisolaE <sinmisolaajao@gmail.com>
2024-01-10 02:05:09 -0800

    6-load_from_json_file.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,5 +4,6 @@
 
 
 def append_write(filename="", text=""):
+    """ appends a text to the end of a file """
     with open(filename, 'a', encoding='UTF8') as f:
-        return(f.write(text))
+        return (f.write(text))
