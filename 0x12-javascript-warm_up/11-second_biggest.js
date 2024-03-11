7bff6e39a49686b51ac442cd5fbbd54929fff4ab
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-11 15:10:04 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,8 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const a = process.argv.slice(2).map(Number);
-  const second = a.sort(function (a, b) { return b - a; })[1];
+  const second = a.sort(function (a, b) {
+    return b - a;
+  })[1];
   console.log(second);
 }	
