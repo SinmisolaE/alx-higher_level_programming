f1ae00a1112e502beb43f203ef915b87b6d103b3
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-11 14:39:38 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,7 +4,7 @@ function factorial (x) {
   if (isNaN(x) || x === 0) {
     return (1);
   }
-  return (x * factorial (x - 1));
+  return (x * factorial(x - 1));
 }
 
 console.log(factorial(Number(process.argv[2])));
