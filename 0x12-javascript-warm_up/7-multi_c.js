e583254f5c4893bcc4268ee89b12ff1bb6815279
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-11 14:23:35 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ let x = process.argv[2];
 if (isNaN(x) || x === undefined) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < parseInt(x); i++) {
+  for (let i = 0; i < Number(x); i++) {
     console.log('C is fun');
   }
 }
