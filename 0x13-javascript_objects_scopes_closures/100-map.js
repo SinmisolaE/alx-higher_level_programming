5e393dd4f11d7ea6d1ed21031eb37901408454e6
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-12 19:37:03 -0700

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-list = require('./100-data.js').list;
+const list = require('./100-data.js').list;
 
 console.log(list);
-console.log(list.map(item, index) => item * index);
+console.log(list.map((item, index) => item * index));
