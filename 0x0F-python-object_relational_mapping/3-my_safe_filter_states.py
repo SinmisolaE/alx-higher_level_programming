7c8718855f686fe62b153c9f067de3246c2ae0fb
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-15 13:22:49 +0000

    5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" takes in argument and displays all values where name matche the argument
+    It should be safe from SQL injection"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2]
+                         db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    search = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY states.id", (search, ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
