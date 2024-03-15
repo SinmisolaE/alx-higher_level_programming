8a04fbe7642e77ec23fe8bc6644d6b9ea4512a14
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-15 19:47:16 +0000

    10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" lists all State objects from the database 'hbtn_0e_6_usa'"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for sect in session.query(State).order_by(states.id):
+        print(sect.id, sect.name, sep=": ")
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""contains the class definition of a State and an instance Base """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+class State(Base):
+    """ Inherits from Base, links to MysQL
+        class attribute id: auto generated, unique integer, can't be null, primary key
+        class attribute name: contain name(max 128 char)
+        """
+        __table_name__ = 'states'
+        id = Column(Integer, unique=True, nullable=Flase, primary_key=True)
+        name = Column(String(128), nullable=False)
