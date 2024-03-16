2df32123b8a66ad11fc4feb05a45b10827841fea
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-16 15:46:50 -0700

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,6 @@ import sys
 
 
 if __name__ == '__main__':
-
     db = MySQLdb.connect(host="loclahost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], host= 3306)
     cur = db.cursor()
