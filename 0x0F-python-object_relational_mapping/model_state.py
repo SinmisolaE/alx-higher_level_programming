e306ff20065e349922b1114b7c9d019b7280a739
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-16 16:52:43 -0700

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,11 +7,13 @@ from sqlalchemy.ext.declarative import declarative_base
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
+
 class State(Base):
     """ Inherits from Base, links to MysQL
-        class attribute id: auto generated, unique integer, can't be null, primary key
+        class attribute id: auto generated,
+        unique integer, can't be null, primary key
         class attribute name: contain name(max 128 char)
         """
-        __table_name__ = 'states'
-        id = Column(Integer, unique=True, nullable=Flase, primary_key=True)
-        name = Column(String(128), nullable=False)
+    __table_name__ = 'states'
+    id = Column(Integer, unique=True, nullable=Flase, primary_key=True)
+    name = Column(String(128), nullable=False)
