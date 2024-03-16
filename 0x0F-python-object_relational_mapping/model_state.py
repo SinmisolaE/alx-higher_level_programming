e33ff117e3b6bfa048918ca350d057f96004e504
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-16 16:54:59 -0700

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """contains the class definition of a State and an instance Base """
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
 mymetadata = MetaData()
