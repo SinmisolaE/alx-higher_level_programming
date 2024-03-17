679bf642f848ca86ea93b200f7b364252ca50141
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-16 17:55:21 -0700

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ lists all State objects with name passed as argument
     should display the states.id
-    """
+"""
 import sys
 from models_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
