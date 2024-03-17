5fa803fcc26fda4355c50b95beed51623b3606bf
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-16 17:44:40 -0700

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,11 +10,11 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metabase.create_all(engine)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    ins = session.query(State).filter(State.name == (sys.argv[4],))
-    try:
-        print(ins[0].id)
-    except IndexError:
+    ins = session.query(State).filter(State.name == (sys.argv[4])).first()
+    if ins is not None:
+        print(ins.id)
+    else:
         print("Not found")
