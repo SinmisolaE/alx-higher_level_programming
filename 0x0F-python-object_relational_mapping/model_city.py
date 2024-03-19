03753a1fedf265699a53494519fb8d30e3f23558
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-18 19:13:54 -0700

    debugged

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" defines class City
-"""
+""" defines class City"""
 from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
