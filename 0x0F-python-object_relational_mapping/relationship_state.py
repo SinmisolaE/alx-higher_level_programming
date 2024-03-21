a1887224421a10df567d9b8e0d58449e18664d94
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-21 04:42:50 -0700

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""contains the class definition of a State and an instance Base """
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """ Inherits from Base, links to MysQL
+        class attribute id: auto generated,
+        unique integer, can't be null, primary key
+        class attribute name: contain name(max 128 char)
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
