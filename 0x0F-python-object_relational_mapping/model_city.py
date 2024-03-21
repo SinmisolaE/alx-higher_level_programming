0e4bd17e152f261692dda08f478e45f29693b5c2
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-21 15:21:47 -0700

    102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,9 +4,6 @@ from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
-
 
 class City(Base):
     """ class City inherits from Base
@@ -17,4 +14,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
