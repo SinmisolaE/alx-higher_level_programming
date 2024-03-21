1e85e354aa8832b45f41ad57a54805ce8cdb0a33
SinmisolaE <sinmisolaajao@gmail.com>
2024-03-21 15:55:43 -0700

    102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -8,7 +8,7 @@ from relationship_city import City
 import sys
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mydqldb://{}:{}@localhost:3306/{}'.
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
