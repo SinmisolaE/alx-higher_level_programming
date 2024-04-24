9f592875a116379cb7b4bc6bebcb3ff85691bf03
SinmisolaE <sinmisolaajao@gmail.com>
2024-04-24 15:07:49 -0700

    101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// prints all characters of a Star Wars movie
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request.get(url, function(err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const content = JSON.parse(body);
+    content.characters.forEach((character) => {
+      request.get(character, function(error, resp, body1) {
+        if (error) {
+	  console.log(error);
+	} else {
+          const cont = JSON.parse(body1);
+	  console.log(cont.name);
+	}
+      });
+    });
+  }
+});
